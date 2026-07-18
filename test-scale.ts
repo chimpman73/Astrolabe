26d9c238a8fe3ddf546fcc3ec3e37f55590bd1c0
chimpman <chimpman.geo@yahoo.com>
2026-07-18 16:07:31 -0700

    Clean up root directory, removing unused test and refactoring scripts

--- test-scale.ts
+++
@@ -1,4 +0,0 @@
-import { ScaleManager } from './src/renderer/utils/ScaleManager';
-
-const renderSize = ScaleManager.getNavChartVisualRadius('J', 56, 'AU', 10);
-console.log('renderSize:', renderSize);
--- test-tree.ts
+++
@@ -1,4 +0,0 @@
-import { LivingWorldRenderer } from './src/renderer/renderers/LivingWorldRenderer';
-import { SizeClass } from './src/types/astrolabe';
-
-console.log("If this compiles, we can test.");
